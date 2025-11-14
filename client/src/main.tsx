09b29660c11b3163769b716b36d039a6be58c32d
corneeme <93950611+corneeme@users.noreply.github.com>
2025-11-14 05:12:44 +0000

    chore: switch to hash routing for Wouter to support GitHub Pages

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,5 +1,11 @@
 import { createRoot } from "react-dom/client";
+import { Router as WouterRouter } from "wouter";
+import { useHashLocation } from "wouter/use-hash-location";
 import App from "./App";
 import "./index.css";
 
-createRoot(document.getElementById("root")!).render(<App />);
+createRoot(document.getElementById("root")!).render(
+	<WouterRouter hook={useHashLocation}>
+		<App />
+	</WouterRouter>,
+);
